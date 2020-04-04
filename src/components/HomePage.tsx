5d55808b0b922fd0fbbdf125591ecb9dee36057d
saujanya <saujanyanagpal104@gmail.com>
2020-04-04 16:06:29 +0530

    deleted

--- src/components/HomePage.tsx
+++
@@ -1,3 +0,0 @@
-import * as React from 'react'
-
-export const HomePage: Function = () => <h1>HomePage</h1>
--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-
-import { HomePage } from './components/HomePage'
-
-ReactDOM.render(<HomePage />, document.getElementById('root'))
