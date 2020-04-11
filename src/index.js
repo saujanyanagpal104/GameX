bd764cbf52b83203af0ef1fb881c0f33a635cf89
saujanya_nagpal <saujanyanagpal104@gmail.com>
2020-04-11 19:50:58 +0530

    test commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { rootReducer } from './reducers'
-import Routes from './Routes'
-import './styles/index.scss'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { rootReducer } from './reducers';
+import Routes from './Routes';
+import './styles/index.scss';
 
-const preloadedState = {}
+const preloadedState = {};
 
-const store = createStore(rootReducer, preloadedState, applyMiddleware(thunk))
+const store = createStore(rootReducer, preloadedState, applyMiddleware(thunk));
 
 render(
     <Provider store={store}>
         <Routes />
     </Provider>,
     document.getElementById('root')
-)
+);
