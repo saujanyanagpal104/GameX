f30897faf4a30cfb80dea6f025949bf195d585b3
saujanya_nagpal <saujanyanagpal104@gmail.com>
2020-04-13 00:12:37 +0530

    style fixes

--- src/components/AboutApp.js
+++ src/components/AboutApp.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AboutApp = () => (
     <div className="about-app">
-        <h1>GameEx</h1>
+        <h1>GameX</h1>
         <span className="app-description">Exchange games. Save money.</span>
     </div>
 );
