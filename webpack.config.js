9f0c9e017dd67fc4a880a6207dcec478f6a5f4e2
saujanyanagpal104 <saujanyanagpal104@gmail.com>
2020-06-22 18:19:38 +0530

    webpack perf changes

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         publicPath: '/',
     },
     module: {
@@ -44,6 +46,27 @@ module.exports = {
     },
     optimization: {
         usedExports: true,
+        runtimeChunk: 'single',
+        splitChunks: {
+            chunks: 'all',
+            maxInitialRequests: Infinity,
+            minSize: 0,
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name(module) {
+                        // get the name. E.g. node_modules/packageName/not/this/part.js
+                        // or node_modules/packageName
+                        const packageName = module.context.match(
+                            /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+                        )[1];
+
+                        // npm package names are URL-safe, but some servers don't like @ symbols
+                        return `npm.${packageName.replace('@', '')}`;
+                    },
+                },
+            },
+        },
     },
     devtool: 'inline-source-map',
     devServer: {
@@ -59,5 +82,6 @@ module.exports = {
             filename: './index.html',
         }),
         new webpack.HotModuleReplacementPlugin(),
+        new webpack.HashedModuleIdsPlugin(),
     ],
 };
